882f3d88ed6336fb7cb2d2e07ba5ddfd63140fc5
pratyushdev-codes <birole.pratyush@gmail.com>
2023-06-16 16:22:26 +0530

    Navbar, Users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import GameComponent from './Components/GameComponent';
+import Information from './Components/Information';
+import Navbar from './Components/Navbar';
+import Directions from './Components/Directions';
 
 function App() {
   return (
     <div className="App">
+    <Navbar/>
     <GameComponent/>
+    <Information/>
+    <Directions/>
     </div>
   );
 }
--- src/Components/GameComponent.js
+++ src/Components/GameComponent.js
@@ -1,120 +1,43 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState } from 'react';
 
 const GameComponent = () => {
   const [number, setNumber] = useState(1);
 
-  useEffect(() => {
-    const dices = ['', '&#9856', '&#9857', '&#9858', '&#9859', '&#9860', '&#9861'];
-    let a;
-    let j;
-    let n = 1;
-    let m = 0;
-    let id = 100;
-
-    for (let a = 0; a < 5; a++) {
-      for (let b = 0; b <= 9; b++) {
-        document.getElementById("out").innerHTML += "<div class='boardbox left' id='box" + id + "' ></div>";
-        id--;
-      }
-      for (let c = 0; c <= 9; c++) {
-        document.getElementById("out").innerHTML += "<div class='boardbox right'id='box" + id + "'></div>";
-        id--;
-      }
-    }
-    document.getElementById("box" + number).innerHTML = "<img id='counter' class='img' src='http://www.freepngimg.com/thumb/chess/9-chess-pawn-png-image-thumb.png'></img>";
-
-    return () => {
-      clearInterval(id);
-    };
-  }, []);
-
   const play = () => {
     setNumber(1);
-    document.getElementById("out").style.visibility = "visible";
-    document.getElementById("dice").style.visibility = "visible";
-    document.getElementById("Play").style.visibility = "hidden";
-    document.getElementById("dice").innerHTML = '&#9856';
   };
 
   const random = () => {
-    const dices = ['', '&#9856', '&#9857', '&#9858', '&#9859', '&#9860', '&#9861'];
-    let a = Math.ceil(Math.random() * 6);
-    document.getElementById("dice").innerHTML = dices[a];
+    const diceValues = ['', '⚀', '⚁', '⚂', '⚃', '⚄', '⚅'];
+    const diceRoll = Math.ceil(Math.random() * 6);
 
-    if (number + a > 100) {
-      a = 100 - number;
+    const newNumber = number + diceRoll <= 100 ? number + diceRoll : number;
+
+    setNumber(newNumber);
+    document.getElementById('dice').innerHTML = diceValues[diceRoll];
+  };
+
+  const renderBoardBoxes = () => {
+    const boardBoxes = [];
+
+    for (let i = 100; i >= 1; i--) {
+      const classNames = `boardbox ${i % 10 === 0 ? 'right' : 'left'}`;
+      const imgSrc = i === number ? 'http://www.freepngimg.com/thumb/chess/9-chess-pawn-png-image-thumb.png' : '';
+
+      boardBoxes.push(
+        <div className={classNames} key={i}>
+          {imgSrc && <img className="img" src={imgSrc} alt="" />}
+        </div>
+      );
     }
 
-    let d = 0;
-    move();
-
-    function move() {
-      setNumber((prevNumber) => {
-        let newNumber = prevNumber + 1;
-        let m = d + 1;
-
-        if (m < a || m === a) {
-          if (d === a - 1) {
-            switch (newNumber) {
-              case 5:
-                newNumber = 25;
-                break;
-              case 10:
-                newNumber = 29;
-                break;
-              case 22:
-                newNumber = 41;
-                break;
-              case 28:
-                newNumber = 55;
-                break;
-              case 44:
-                newNumber = 95;
-                break;
-              case 70:
-                newNumber = 89;
-                break;
-              case 79:
-                newNumber = 81;
-                break;
-              case 99:
-                newNumber = 7;
-                break;
-              case 92:
-                newNumber = 35;
-                break;
-              case 73:
-                newNumber = 53;
-                break;
-              case 78:
-                newNumber = 39;
-                break;
-              case 37:
-                newNumber = 17;
-                break;
-              case 31:
-                newNumber = 14;
-                break;
-              default:
-                newNumber = newNumber;
-            }
-          }
-          d += 1;
-        } else {
-          m = 0;
-          newNumber -= 1;
-          clearInterval(move);
-        }
-
-        return newNumber;
-      });
-    }
+    return boardBoxes;
   };
 
   return (
-    <div>
-      <style>{`
+    <div style={{ textAlign: 'center' }}>
+      <style>
+        {`
         h2 {
           text-align: center;
           background-color: #11143F;
@@ -145,8 +68,9 @@ const GameComponent = () => {
           height: 40px;
           width: 100px;
           margin-left: 50px;
-          margin-top: 20px;
+          margin-top: 50px;
           border-radius: 5px;
+          color:white
         }
         .boardbox {
           height: 50px;
@@ -168,15 +92,16 @@ const GameComponent = () => {
           height: 80px;
           width: 80px;
           font-size: 70px;
-          color: black;
+          color: white;
           margin: auto;
           border: none;
-          background-color: white;
+          background-color:black;
         }
-      `}</style>
+      `}
+      </style>
       <h2>ऐLan</h2>
-      <div className="out" id="out"></div>
-      <div className="mn" id="out"></div>
+      <div className="out">{renderBoardBoxes()}</div>
+      <div className="mn"></div>
       <div id="side">
         <button className="btn" id="Play" onClick={play}>
           New game
